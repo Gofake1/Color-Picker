55a868f8fb812b90fbe70237938f8aec64cc752f
David Wu <david@gofake1.net>
2017-05-04 20:11:32 -0700

    Initial Commit

---
+++ Color Picker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Color Picker
+//
+//  Created by David Wu on 5/4/17.
+//  Copyright © 2017 Gofake1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Color Picker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Color Picker
+//
+//  Created by David Wu on 5/4/17.
+//  Copyright © 2017 Gofake1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
