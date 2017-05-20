ebb7fd35e3fd2784996906183022b4b6eb89a5d3
David Wu <david@gofake1.net>
2017-05-19 20:12:50 -0700

    Implemented drag and drop reordering for palettes

--- Color Picker/PaletteViewController.swift
+++ Color Picker/PaletteViewController.swift
@@ -14,9 +14,12 @@ class PaletteViewController: NSViewController {
     @IBOutlet weak var collectionView: NSCollectionView!
     // Injected by AppDelegate
     dynamic weak var paletteCollection: PaletteCollection!
+    /// Cached index paths for dragged items in the current drag session
+    fileprivate var draggedIndexPaths = Set<IndexPath>()
 
     override func viewDidLoad() {
         collectionView.register(PaletteCollectionViewItem.self, forItemWithIdentifier: "palette")
+        collectionView.register(forDraggedTypes: ["net.gofake1.Color-Picker.palette"])
     }
 
     override func deleteBackward(_ sender: Any?) {
@@ -34,5 +37,50 @@ class PaletteViewController: NSViewController {
             collectionController.filterPredicate = NSPredicate(format: "name contains[cd] %@",
                                                                argumentArray: [sender.stringValue])
         }
+    }
+}
+
+extension PaletteViewController: NSCollectionViewDelegate {
+
+    func collectionView(_ collectionView: NSCollectionView,
+                        pasteboardWriterForItemAt indexPath: IndexPath) -> NSPasteboardWriting? {
+        let writer = NSPasteboardItem()
+        let data = NSKeyedArchiver.archivedData(withRootObject: paletteCollection.palettes[indexPath.item])
+        writer.setData(data, forType: "net.gofake1.Color-Picker.palette")
+        return writer
+    }
+
+    func collectionView(_ collectionView: NSCollectionView,
+                        draggingSession session: NSDraggingSession,
+                        willBeginAt screenPoint: NSPoint,
+                        forItemsAt indexPaths: Set<IndexPath>) {
+        draggedIndexPaths = indexPaths
+    }
+
+    func collectionView(_ collectionView: NSCollectionView,
+                        draggingSession session: NSDraggingSession,
+                        endedAt screenPoint: NSPoint,
+                        dragOperation operation: NSDragOperation) {
+        draggedIndexPaths = []
+    }
+
+    func collectionView(_ collectionView: NSCollectionView,
+                        validateDrop draggingInfo: NSDraggingInfo,
+                        proposedIndexPath proposedDropIndexPath: AutoreleasingUnsafeMutablePointer<NSIndexPath>,
+                        dropOperation proposedDropOperation: UnsafeMutablePointer<NSCollectionViewDropOperation>)
+        -> NSDragOperation {
+            return .move
+    }
+
+    func collectionView(_ collectionView: NSCollectionView,
+                        acceptDrop draggingInfo: NSDraggingInfo,
+                        indexPath: IndexPath,
+                        dropOperation: NSCollectionViewDropOperation) -> Bool {
+        for fromIndexPath in draggedIndexPaths {
+            let temp = paletteCollection.palettes.remove(at: fromIndexPath.item)
+            paletteCollection.palettes.insert(temp, at: (indexPath.item <= fromIndexPath.item)
+                ? indexPath.item : indexPath.item-1)
+        }
+        return true
     }
 }
